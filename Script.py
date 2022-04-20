be99a58243032e084fb3cf2be1314eba02c27cfb
ERROR X PREDATOR <86637047+Alinshan@users.noreply.github.com>
2022-04-20 19:28:30 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,25 +5,24 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝘏𝘦𝘳𝘦 𝘐𝘴 𝘛𝘩𝘦 𝘏𝘦𝘭𝘱 𝘍𝘰𝘳 𝘔𝘺 𝘊𝘰𝘮𝘮𝘢𝘯𝘥𝘴."""
     ABOUT_TXT = """
-𝗧𝗛𝗜𝗦 𝗔𝗡𝗡𝗔 𝗕𝗘𝗡'𝗦 𝗔𝗕𝗢𝗨𝗧 𝗠𝗦𝗚
+𝗔𝗕𝗢𝗨𝗧 𝗠𝗦𝗚
 ✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
-╔════❰ ꪖ᥇ꪮꪊ𝓽 ꪑ𝘴ᧁ ❱═❍⊱❁۪۪
+╔════❰ ᴅᴀsʜᴀᴍᴏᴏʟᴀᴍ ᴅᴀᴍᴜ ❱═❍⊱❁۪۪
 ║╭━━━━━━━━━━━━━━━➣ 
-║┣⪼ 𝙈𝙔 𝙉𝘼𝙈𝙀 - <a href="https://t.me/Dqautofl_Bot"> 𝑨𝑵𝑵𝑨 𝑩𝑬𝑵 </a>
-║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎1 - <a href="https://t.me/pro_editor_tg"> Lalluᵗᵍ </a>
-║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎2 - <a href="https://t.me/PANDITHAN_SIR"> ⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋM-STER </a>
-║┣⪼ 𝓛𝓲𝓫𝓻𝓪𝓻𝓻𝔂 - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-║┣⪼ 𝓛𝓪𝓷𝓰𝓾𝓪𝓰𝓮 - 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-║┣⪼ 𝓓𝓪𝓽𝓪 𝓑𝓪𝓼𝓮 - 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-║┣⪼ 𝓑𝓸𝓽 𝓼𝓮𝓻𝓿𝓮𝓻 -  𝙷𝙴𝚁𝙾𝙺𝚄
-║┣⪼ 𝓑𝓾𝓲𝓵𝓭 𝓢𝓽𝓪𝓽𝓾𝓼 - v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
+║┣⪼ 𝗡𝗔𝗠𝗘 - <a href="https://t.me/TonySerBot"> 𝗗𝗔𝗦𝗛𝗔𝗠𝗢𝗢𝗟𝗔𝗠 𝗗𝗔𝗠𝗨 </a>
+║┣⪼ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥 - <a href="https://t.me/ERROR_X_PREDATOR">𝗔𝗟𝗜𝗡𝗦𝗛𝗔𝗡ᵗᵍ </a>
+║┣⪼ 𝗟𝗜𝗕𝗥𝗔𝗥𝗥𝗬 - 𝗣𝗬𝗥𝗢𝗚𝗥𝗔𝗠
+║┣⪼ 𝗟𝗔𝗡𝗚𝗨𝗔𝗚𝗘 - 𝗣𝗬𝗧𝗛𝗢𝗡 𝟯
+║┣⪼ 𝗗𝗔𝗧𝗔 𝗕𝗔𝗦𝗘 - 𝗠𝗢𝗡𝗚𝗢 𝗗𝗕
+║┣⪼ 𝗕𝗢𝗧 𝗦𝗘𝗥𝗩𝗘𝗥 -  𝗛𝗘𝗥𝗢𝗞𝗨
+║┣⪼ 𝗕𝗨𝗜𝗟𝗗 𝗦𝗧𝗔𝗧𝗨𝗦 - 𝘃𝟭.𝟬.𝟭 [ 𝗕𝗘𝗧𝗔 ]
 ║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪"""
     SOURCE_TXT = """<b>NOTE:</b>
 - 𝑰 𝒂𝒎  𝒂 𝒐𝒑𝒆𝒏 𝒔𝒐𝒖𝒓𝒄𝒆 𝒑𝒓𝒐𝒋𝒆𝒄𝒕. 
-- ՏOᑌᖇᑕᗴ ᑕOᗪᗴ - <a href="https://github.com/Lallu-lallus/AnnaBen_robot"> 𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 </a>
+- 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 - <a href="https://github.com/Alinshan/Damu"> 𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 </a>
 
 𝗠𝗔𝗦𝗧𝗘𝗥:
-<a href="https://t.me/team_annaben"> 𝑻𝑬𝑨𝑴 𝑨𝑵𝑵𝑨 </a>"""
+<a href="https://t.me/ERROR_X_PREDATOR"> 𝗔𝗟𝗜𝗡𝗦𝗛𝗔𝗡 </a>"""
     WHOIS_TXT ="""<b>WHOIS MODULE</b>
 Note:- Give a user details
 
